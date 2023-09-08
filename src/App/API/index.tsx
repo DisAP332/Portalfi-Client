e26a1fe847763814ad0a90a49ad6fb2f97f4655f
DAP <disap332@gmail.com>
2023-09-08 16:01:46 -0400

    changed baseURL

--- src/App/API/index.tsx
+++ src/App/API/index.tsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import Cookies from '../Cookies'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'http://portalfi-jbw.com:8080/'
 })
 
 // user helpers //
